68a889cf91d0dada658260fe65d156f4f6e0a808
yuliyamkh <yuliya.mkhayan@gmail.com>
2024-04-15 14:35:24 +0200

    Add classification report and confusion matrix

--- training_model.py
+++ training_model.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
 from data_exploration import save_plot_as_image
 import pandas as pd
 import numpy as np
@@ -7,6 +8,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import RepeatedStratifiedKFold
 from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 
 
 def tune_penalty_hyperparameter(parameter_array):
@@ -52,7 +56,7 @@ if __name__ == '__main__':
     # targets = pd.get_dummies(targets, dtype=int)
 
     # Split data into train and test sets
-    X_train, X_test, y_train, y_test = train_test_split(features, targets, test_size=0.1, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(features, targets, test_size=0.2, random_state=42, shuffle=True)
 
     # Define a model
     model = LogisticRegression(multi_class='multinomial', solver='lbfgs')
@@ -70,6 +74,7 @@ if __name__ == '__main__':
 
     # Report the model performance
     print(f"Mean Accuracy: {np.round(np.mean(n_scores), 2)}, {np.round(np.std(n_scores), 2)}")
+    print('\n')
 
     # Tune penalty for multinomial logistic regression
     models = tune_penalty_hyperparameter(parameter_array=[0.0, 0.0001, 0.001, 0.01, 0.1, 1.0])
@@ -84,3 +89,41 @@ if __name__ == '__main__':
     plt.boxplot(results, labels=names, showmeans=True)
     save_plot_as_image(folder='images', image_name='L2_regularization')
     plt.show()
+
+    # Train model with the best penalty parameter
+    model = LogisticRegression(multi_class='multinomial', solver='lbfgs', penalty='l2', C=0.001)
+
+    # Fit the model
+    model.fit(X_train, y_train)
+
+    # Check the performance of the model on train data
+    y_pred_train = model.predict(X_train)
+    accuracy_train = accuracy_score(y_train, y_pred_train)
+    print('\n')
+    print(f'Accuracy on train data: {np.round(accuracy_train, 2)}')
+
+    # Check the performance of the model on test data
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    print('\n')
+    print(f'Accuracy on test data: {np.round(accuracy, 2)}')
+
+    # Generate classification report
+    print('\n')
+    print("Classification Report:")
+    print(classification_report(y_test, y_pred))
+
+    # Compute confusion matrix of test target classes and predictions produced by the model
+    cm = confusion_matrix(y_test, y_pred)
+    classes = np.unique(y_test)
+
+    # Plot confusion matrix
+    # plt.figure(figsize=(8, 6))
+    sns.set(font_scale=1)
+    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", xticklabels=classes, yticklabels=classes)
+    plt.xlabel('Predicted labels')
+    plt.ylabel('True labels')
+    plt.title('Confusion Matrix')
+    save_plot_as_image('images', 'confusion_matrix')
+    plt.show()
+
